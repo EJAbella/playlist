df1f4d97eef80a2ae5410e938c56c882814a9ddf
Elijah <elijah.abella@aol.com>
2018-07-12 18:56:22 -0700

    Increase responsiveness and more user friendly when adding songs

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const splashOne = document.querySelector('#splashOne')
 const splashTwo = document.querySelector('#splashTwo')
 const splashThree = document.querySelector('#splashThree')
 const albumScroller = document.querySelector('.album-scroller')
-const addAlbums = document.querySelector('.addAlbums')
 const clearTracks = document.querySelector('.clear-tracks')
 const submitBin = document.querySelector('.submit-bin')
 const postResponse = document.querySelector('#postResponse')
+const currentAlbum = document.querySelector('.currentAlbum')
+const trackList = document.querySelector('.trackList')
+const playlistBin = document.querySelector('.playlistBin')
+const playlistBin2 = document.querySelector('.playlistBin2')
 
 window.addEventListener('load', () => {
-  axios.get('https://lit-fortress-6467.herokuapp.com/object ')
+  axios.get('http://localhost:3000/posts')
     .then(response => {
-      const data = response.data.results
-      splashOne.src = `images/${data[0].cover_art}`
-      splashTwo.src = `images/${data[2].cover_art}`
-      splashThree.src = `images/${data[3].cover_art}`
+      const data = response.data
+      splashOne.src = `${data[0].image}`
+      splashTwo.src = `${data[2].image}`
+      splashThree.src = `${data[3].image}`
     })
 })
 
@@ -21,7 +24,6 @@ window.addEventListener('load', () => {
   axios.get('http://localhost:3000/posts')
     .then(response => {
       const data = response.data
-      console.log(response)
       for(let i=0; i<data.length; i++) {
         albumScroller.innerHTML += `<div class='album_container'><img id='${i}' class='album' src=${data[i].image}></div>`
       }
@@ -32,24 +34,41 @@ albumScroller.addEventListener('click', (e) => {
   axios.get('http://localhost:3000/posts')
     .then(response => {
       const data = response.data
-      addAlbums.innerHTML += `<p>${data[(e.target).id].author}: ${data[(e.target).id].title}</p><hr>`
+      currentAlbum.style.paddingTop = '0px'
+      currentAlbum.innerHTML = `<p>Add tracks from:<br><b>${data[(e.target).id].author}: ${data[(e.target).id].title}</b></p><img class='albumChoice' src='${data[(e.target).id].image}'<hr>`
+      let list = `<h4 class='boxTitle'>Album Songs</h4>`
+      for(let i=0; i<data[(e.target).id].songs.length; i++) {
+        list += `<p>${data[(e.target).id].songs[i]}<hr></p>`
+      }
+      trackList.innerHTML = list;
     })
 })
 
+trackList.addEventListener('click', (e) => {
+  if (e.target.tagName === 'P') {
+    let newSong = document.createElement("P")
+    let line = document.createElement("HR")
+    newSong.textContent = e.target.textContent
+    playlistBin2.prepend(line)
+    playlistBin2.prepend(newSong)
+  }
+})
+
 clearTracks.addEventListener('click', () => {
-  addAlbums.innerHTML = ''
+  playlistBin2.innerHTML = ''
 })
 
 submitBin.addEventListener('click', () => {
   axios.post('https://lit-fortress-6467.herokuapp.com/post',
   {
-    addAlbums: addAlbums.textContent
+    addAlbums: playlistBin.textContent
   })
     .then(response => {
       postResponse.style.backgroundColor = 'hsl(140, 50%, 50%)'
       postResponse.style.animationName = 'fadeIn'
       postResponse.style.animationDuration = '3s';
       postResponse.textContent = `playlist submitted successfully!`
+      playlistBin2.innerHTML = ''
       setTimeout(function() {
         postResponse.style.animationName = ''
         postResponse.style.animationDuration = '';
